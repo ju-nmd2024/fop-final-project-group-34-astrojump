2ebcd0504dbd7b3e610a211a669fdc15f86056cd
eridenius <eridenius@gmail.com>
2024-11-25 16:02:13 +0100

    Update game.js
    character values

--- game.js
+++ game.js
@@ -1,7 +1,32 @@
+//character class
+
+class Character {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2; 
+    this.width = 30;
+    this.height = 30;
+    this.vertX = 0;
+    this.vertY = 0;
+    this.gravity = 0.5;
+    this.jumpForce = -10;
+    this.speed = 5;
+  }
+ 
+
+
+
 function setup() {
   createCanvas(800, 600);
+  myCharacter = new Character();
 }
 
 function draw() {
-  background(255, 140, 0);
+  background(255, 255, 255);
+  myCharacter.draw();
+  
 }
+
+
+
+
