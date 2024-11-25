12bb6928607ce7acab7f90d4f05325e0ef240c35
h4shBrowns <fohe24kc@student.ju.se>
2024-11-25 16:36:14 +0100

    Add gravity and comments
    Added gravity to the character and made it stop if it moved too far down. Also added some comments to explain the code

--- game.js
+++ game.js
@@ -2,17 +2,20 @@
 
 class Character {
   constructor() {
-    this.x = width / 2;
-    this.y = height / 2;
+    this.x = 400 / 2;
+    this.y = 600 / 2;
     this.width = 30;
     this.height = 30;
+    
     this.velX = 0;
     this.velY = 0;
-    this.gravity = 0.5;
+    this.gravity = 1.0;
     this.jumpForce = -10;
     this.speed = 7;
   }
 
+
+  // Character moves if player presses left or right arrows
   move() {
     if (keyIsDown(LEFT_ARROW)) {
       this.velX = -this.speed;
@@ -23,7 +26,30 @@ class Character {
     }
 
     this.x += this.velX;
+
+    // Asked chatgpt for horizontal character screen wrapping
+    // https://chatgpt.com/share/67449514-b470-8001-b741-23c701b91a3b
+
+    if (this.x > 400) {
+      this.x = 0; 
+    } else if (this.x < 0) {
+      this.x = 400;
+    }
   }
+
+  // Apply gravity & stop if character moves too far
+  update() {
+    this.velY += this.gravity;
+
+    this.y += this.velY;
+
+    if (this.y > 570) {
+      this.gravity = 0;
+      this.velY = 0;
+    }
+  }
+
+  
 
   draw() {
     fill(255, 0, 0);
@@ -32,7 +58,7 @@ class Character {
 }
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(400, 600);
   player = new Character();
 }
 
@@ -40,4 +66,5 @@ function draw() {
   background(255, 255, 255);
   player.draw();
   player.move();
+  player.update();
 }
