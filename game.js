85cb693265fb71d1e02b1b31e5471107ff740cf4
eridenius <eridenius@gmail.com>
2024-11-25 16:09:00 +0100

    Update game.js
    added horizontal movement for the character.

--- game.js
+++ game.js
@@ -3,30 +3,41 @@
 class Character {
   constructor() {
     this.x = width / 2;
-    this.y = height / 2; 
+    this.y = height / 2;
     this.width = 30;
     this.height = 30;
-    this.vertX = 0;
-    this.vertY = 0;
+    this.velX = 0;
+    this.velY = 0;
     this.gravity = 0.5;
     this.jumpForce = -10;
-    this.speed = 5;
+    this.speed = 7;
   }
- 
 
+  move() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.velX = -this.speed;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+      this.velX = this.speed;
+    } else {
+      this.velX = 0;
+    }
 
+    this.x += this.velX;
+  }
+
+  draw() {
+    fill(255, 0, 0);
+    ellipse(this.x, this.y, this.width, this.height);
+  }
+}
 
 function setup() {
   createCanvas(800, 600);
-  myCharacter = new Character();
+  player = new Character();
 }
 
 function draw() {
   background(255, 255, 255);
-  myCharacter.draw();
-  
+  player.draw();
+  player.move();
 }
-
-
-
-
